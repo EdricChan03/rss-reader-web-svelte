5e8035d5a1397aaa387cedc3b16afbe2dbbb639f
Edric <edric.chan.1997@gmail.com>
2023-08-15 21:10:29 +0800

    ci(gh-pages): use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const isCi = !!process.env.CI;
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1] ?? 'rss-reader-web-svelte';
+const kitBasePath = isCi ? `/${repoName}` : '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +15,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: kitBasePath
+    }
 	}
 };
 
